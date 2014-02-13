f9acd10f38284e08814c971a9e15e8b959d69d1d
toomz <eleonore.pirri@gmail.com>
2014-02-13 22:24:02 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BazarDuCitron::Application.config.secret_key_base = 'b9a6ca9f848c03265c4d9ee2ea3b8e00e33a44ad1a5e1cf405d9d931cd65a92e4395758c6acc08346762c6334192521519c4db8b907692866564d13e8c167d3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BazarDuCitron::Application.config.session_store :cookie_store, key: '_bazar-du-citron_session'
